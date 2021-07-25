10144295ce3ecc3fad5025e1d35b73a974a04ecc
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-07-25 18:08:43 -0400

    Populator frame built|applications.properties data added

--- src/main/java/Populator.java
+++ src/main/java/Populator.java
@@ -1,1 +1,23 @@
-public class Populator { }
+import com.wildernesswild.wildernesswild.repositories.RegionsRepository;
+import com.wildernesswild.wildernesswild.repositories.ScenariosRepository;
+import com.wildernesswild.wildernesswild.repositories.SkillsRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class Populator implements CommandLineRunner {
+
+    @Resource
+    private RegionsRepository regionsRepo;
+    @Resource
+    private ScenariosRepository scenariosRepo;
+    @Resource
+    private SkillsRepository skillsRepo;
+
+    @Override
+    public void run(String... args) throws Exception {
+        /*Code goes in here*/
+    }
+}
