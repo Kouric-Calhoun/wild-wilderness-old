72dfa532386c6fc5492b54a3eb9c64a23520459a
FilsanMadar <filsanmadar@gmail.com>
2021-07-29 19:24:45 -0400

    about us

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -1,4 +1,109 @@
 export default function About() {
   return `
-    <h1>About us Test page! Kouric put your code in here!</h1>`;
+    <div class="container">
+    <div class="title">
+      <h1>About The Developers</h1>
+    </div>
+    <div class="about-content">
+
+      <div class="card" id="one">
+        <div class="img_box">
+          <img src="" height="300px" width="200px" alt="Photo">
+          <div class="img_info">
+            <h4>Java FullStack</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, natus beatae corrupti, enim neque optio totam veritatis nobis non dolorum soluta similique temporibus odit animi sapiente voluptatum quos. Voluptates, deserunt.</p>
+            <div class="social">
+              <a href="" class="fa fa-linkedin"></a>
+              <a href="" class="fa fa-github"></a>
+            </div>
+          </div>
+
+        </div>
+        <h3>Filsan Madar</h3>
+      </div>
+
+
+      <div class="card" id="two">
+        <div class="img_box">
+          <img src="" height="300px" width="200px" alt="Photo">
+          <div class="img_info">
+            <h4>Java FullStack</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto eligendi, minus modi delectus obcaecati quod, veniam nesciunt velit ratione error quis voluptas earum blanditiis quia ex voluptate officia rem mollitia.</p>
+            <div class="social">
+              <a href="" class="fa fa-linkedin"></a>
+              <a href="" class="fa fa-github"></a>
+            </div>
+          </div>
+
+        </div>
+        <h3>Kouric Calhoun</h3>
+      </div>
+
+      <div class="card" id="three">
+        <div class="img_box">
+          <img src="" height="300px" width="250px" alt="Photo">
+          <div class="img_info">
+            <h4>Java FullStack</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa molestiae a facere recusandae incidunt explicabo deleniti eligendi suscipit. Aperiam hic asperiores accusamus tempore labore tenetur ullam quibusdam culpa consequuntur excepturi.</p>
+            <div class="social">
+              <a href="" class="fa fa-linkedin"></a>
+              <a href="" class="fa fa-github"></a>
+            </div>
+          </div>
+
+        </div>
+        <h3>Yuli Elroy</h3>
+      </div>
+
+      <div class="card" id="four">
+        <div class="img_box">
+          <img src="" height="300px" width="270px" alt="Photo">
+          <div class="img_info">
+            <h4>Java FullStack</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id non ab provident esse temporibus iste dicta ipsum cumque consequatur facere nemo debitis, distinctio excepturi ipsam consectetur architecto assumenda corrupti. Facilis?</p>
+            <div class="social">
+              <a href="" class="fa fa-linkedin"></a>
+              <a href="" class="fa fa-github"></a>
+            </div>
+          </div>
+
+        </div>
+        <h3>Ty Jackson</h3>
+      </div>
+
+      <div class="card" id="five">
+        <div class="img_box">
+          <img src="" height="300px" width="270px" alt="Photo">
+          <div class="img_info">
+            <h4>Java FullStack</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ab ratione fugiat deserunt accusamus modi nihil magni voluptate, dolor, nisi accusantium. Dicta est amet vero repellat vitae corrupti odio debitis.</p>
+            <div class="social">
+              <a href="" class="fa fa-linkedin"></a>
+              <a href="" class="fa fa-github"></a>
+            </div>
+          </div>
+
+        </div>
+        <h3>Jovahnn Tucker</h3>
+      </div>
+
+      <div class="card" id="six">
+        <div class="img_box">
+          <img src="" height="300px" alt="Photo">
+          <div class="img_info">
+            <h4>Java FullStack</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa molestiae a facere recusandae incidunt explicabo deleniti eligendi suscipit. Aperiam hic asperiores accusamus tempore labore tenetur ullam quibusdam culpa consequuntur excepturi.</p>
+            <div class="social">
+              <a href="" class="fa fa-linkedin"></a>
+              <a href="" class="fa fa-github"></a>
+            </div>
+          </div>
+
+        </div>
+        <h3>Miranda Perry</h3>
+      </div>
+
+    </div>
+  </div>
+`;
 }
