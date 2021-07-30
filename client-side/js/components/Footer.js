6940201e9b042f1ddd9ff72b45ac637ce139f9e8
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-07-29 21:08:23 -0400

    update content page of about, contact. Fix minor codes on footer, index.js, and index.html

--- client-side/js/components/Footer.js
+++ client-side/js/components/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
   return `
    <ul class="footer">
-    <li>About</li>
-    <li>Contact</li>
+    <li class="footer__about">About</li>
+    <li class="footer__contact">Contact</li>
     </ul>
     <small>&copy Wilderness Wild & WCCI 2021</small>
     `;
