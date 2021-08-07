5e77309116fb09a6bc982ccf7a64ba146a40d205
FilsanMadar <filsanmadar@gmail.com>
2021-08-07 01:16:12 -0400

    created button links for events, chatroom, and gear checklist jspages

--- client-side/js/components/Home.js
+++ client-side/js/components/Home.js
@@ -3,10 +3,22 @@ export default function Home() {
   return `
   <div id="home-div">
     <h1>Wilderness Wild</h1>
-
-    <div class="home-splash"></div>
-    <h3 class="home-p"><em>Preparedness - The Absolute Key To Survival</em></h3>
-    <p>Wilderness Wild is committed to giving our users all the tools they need to survive their nature adventures, while also allowing them to connect with like minded adventures! </p>
+    <div class="home-splash">
+      <h3 class="home-p"><em>Preparedness - The Absolute Key To Survival</em></h3>
+      <p>Wilderness Wild is committed to giving our users all the tools they need to survive their nature adventures, while also allowing them to connect with like minded adventures! </p>
+    </div>
+    <div class="homeCards-Events">
+      <h3>Check Out Events In Your Area</h3>
+      <button class = "home-btn-links">Events</button>
+    </div>
+    <div class="homeCards-Chatrooms">
+      <h3>Chatrooms Are Open!</h3>
+      <button class = "home-btn-links">Chatroom</button>
+    </div>
+    <div class="homeCards-Gear">
+      <h3>Absolute Survival Gear Checklist</h3>
+      <button class = "home-btn-links">Checklist</button>
+    </div>
   </div>  
   `;
 }
