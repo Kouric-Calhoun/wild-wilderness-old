50f4bea6f6d86b68ad7b48e38a7efe064aeab6fb
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-08-06 21:44:42 -0400

    adding buttons in skills from Miranda

--- client-side/js/components/Skills.js
+++ client-side/js/components/Skills.js
@@ -1,16 +1,43 @@
+// import lightingFire from '../../images/lightingFire.jpg';
+import waterProcurement from '../../images/waterProcurement.jpg';
+// import shelterLinkPic from '../../images/shelterLinkPic.jpg';
+// import regionsLinkPic from '../../images/regionsLinkPic.jpg';
+// import scenariosLinkPic from '../../images/scenariosLinkPic.jpg';
+import skill from '../../css/skill';
 
-export default function Skills(skills) {
+export default function Skills() {
   return `
-  <div class='nav-list__skills'>
-    <ul>
-    ${skills.collections.items
-      .map((skills) => {
-        return `
-        <h3>${skills(skills)}</h3>
-        `;
-      })
-      .join('')}
-    </ul>
+    <h1>Survival Skills</h1>
+    <div class ="skills-main">
+      <div class="card">
+        <img class="fire-link-pic" src ='$-{lightingFire}'>
+        <h3>How to safely build a fire</h3>
+        <button class ="skills-btn-links">How to build a Fire</button>
+      </div>
+      
+      <div class="card">
+        <img class="fire-link-pic" src ='${waterProcurement}'>
+        <h3>How to procure water</h3>
+        <button class = "skills-btn-links">Water Procurement</button>
+      </div>
+    
+      <div class="card">
+        <img class="fire-link-pic" src ='$-{shelterLinkPic}'>
+        <h3>How to find/make shelter</h3>
+        <button class = "skills-btn-links">Shelter</button>
+      </div>
+      
+      <div class="card">
+        <img class="fire-link-pic" src ='$-{regionsLinkPic}'>
+        <h3>Explore by Region</h3>
+        <button class = "skills-btn-links">Regions</button>
+      </div>
+    
+      <div class="card">
+        <img class="fire-link-pic" src ='$-{scenariosLinkPic}'>
+        <h3>What to do in specific scenarios</h3>
+        <button class = "skills-btn-links">Scenarios</button>
+      </div>
     </div>
     `;
 }
