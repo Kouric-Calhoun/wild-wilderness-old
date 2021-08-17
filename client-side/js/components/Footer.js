1e435aba2a10f84a7daeead1e776b5ce4feae5ef
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-08-16 21:59:04 -0400

    matching home to landing page

--- client-side/js/components/Footer.js
+++ client-side/js/components/Footer.js
@@ -1,43 +1,11 @@
 export default function Footer() {
   return `
-    <body id="footer">
-    <!-- Section A -->
-    <section id="section-a" class="grid">
-    <div class="content-wrap">
-    <h3>Wilderness Wild</h3>
-    <p>Wilderness Wild is a nonprofit organization dedicated to improving 
-    the lives by providing the trustworthy information, education, and 
-    independent voice related to survival skills in the wilderness.</p>
-    <br>
-    </div>
-    </section>
-    <!-- Section B -->
-    <section id="section-b" class="grid">
-    <div class="content-wrap">
-    <h3>Learn more</h3>
-    <li class="footer__about">About Us</li>
-    <li class="footer__contact">Contact</li>
-    </ul>
-    </div>
-    </section>
-    <!-- Section C -->
-    <section id="section-c" class="grid">
-    <div class="content-wrap">
+    
     <h3>Get Involved</h3>
     <li class="footer__donate">Donate</li>
-    <li class="footer__event">Attend an Event</li>
     <li class="footer__hiring">We Are Hiring</li>
-    </ul>
-    </div>
-    </section>
-      
-    <!-- Section D-->
-    <div class="box">
-    <p><small>&copy Wilderness Wild & WCCI 2021</small></p>  
-    </div>
-    </section>
  
-    </body>
+    <p><small>&copy Wilderness Wild & WCCI 2021</small></p>  
    
     `;
 }
