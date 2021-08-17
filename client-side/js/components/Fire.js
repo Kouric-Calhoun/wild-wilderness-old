238065d7542080bfb203b32ab43c0ce950777630
MirandaPerry1982 <mirandaperry1982@yahoo.com>
2021-08-17 18:06:58 -0400

    skill pages in progress

--- client-side/js/components/Fire.js
+++ client-side/js/components/Fire.js
@@ -15,7 +15,7 @@ export default function Fire() {
     <div class="fire-container">
     <article class = "fire-header">
     <h1 class = "fire-title">How to Safely Build a Fire</h1>
-    <img src='${campfire}' alt = "pic of a campfire">
+    <img class="fire-pic-1" src='${campfire}' alt = "pic of a campfire">
         <p class="fire-text">One of the most important skills to have when you are braving the wilderness is knowing how to build a fire.
     It is also important to know how to safely build this fire and how to tend to it so that you do not accidentally 
    start a wildfire. It is important that you know how to build a fire in the wilderness so that you can keep yourself
@@ -38,11 +38,13 @@ export default function Fire() {
     of the stuff before heading into the wilderness. It contains a natural oil that is water-resistant, so it will 
     catch fire even if it is wet.  If you’re harvesting bark in the backcountry, try to find some that has been blown 
     off of trees. Each piece has many layers, so keep peeling until you find a dry one.</p>
+    
         <p class="fire-text">Spruce sap also makes a great natural firestarter. The flammable substance oozes out of injured trees and hardens 
     into a resin, which can be snapped off and set ablaze. It may take several matches to get going but once lit,
     the sap will burn for several minutes, making it a great resource in wet conditions. While spruce is best, any 
     hardened sap will do. Just find a blob that’s about the size of a wad of chewing gum, pull it off the tree, place 
     it on the end of a stick, and light it up.</p>
+    
         <p class="fire-text">Other substances that make admirable stand-ins for natural firestarters include cotton balls coated in petroleum 
     jelly, hand sanitizer sprinkled on small kindling, such as dry leaves and grass, or the fibrous inside of the 
     bark of cedar trees. You can also make your own fire starters by melting candle wax, adding dryer lint, and
@@ -58,6 +60,7 @@ export default function Fire() {
     If you neglect one of these things, your triangle will collapse. Usually, fuel means wood and an ignition
     source is a match or lighter. The level of oxygen will depend on how the wood is arranged—if you stack it well, it
     will facilitate air movement so the fire can breathe and grow.</p>
+    
         <p class="fire-text">Start with the kindling. You can dig a trench a couple of inches deep as your fire pit, then lay three small pieces
      of wood over the trench in a triangle that’s just big enough to support your tinder. Pile your tinder, place your 
     firestarter of choice in the middle of it, and build your structure around it all. Start with smaller branches and 
@@ -76,6 +79,7 @@ export default function Fire() {
     the lowest parts of nearby trees. Sometimes you have to do what’s necessary to survive. Look for dead trees or 
     branches and wood that’s fallen to the ground that might be soggy on the outside, but dry on the inside. When you
     do, carve off the wet outer layers until you hit dry wood.</p>
+    
         <p class="fire-text">Find a sheltered location that will help you stay dry and warm, like against a rock face or in a grove of trees. 
     In gusty weather, dig a pit for your fire to sit in—the stronger the wind, the deeper your pit should be. Collect
     more wood than you think you need—you don’t want to have to leave your fire. Make your fire structure no more 
@@ -84,8 +88,9 @@ export default function Fire() {
     </div>
     </article>
     
-    <article class = "fire-methods>
-    <h1 class = "fire-methods-title">Methods for starting a fire</h1>
+    <div class = "fire-methods>
+    
+    <h1 class= "fire-methods-title">Methods for starting a fire</h1>
 
     <div class="fire-drill-container">
     <h2 class = "hand-drill-title">The Hand Drill</h2>
@@ -94,18 +99,18 @@ export default function Fire() {
     primitive method and the hardest to do. Shaft revolution and descending weight are two of the most imperative prerequisites 
     for starting a hand drill fire. This method will require wood, resolute hands, and gritty determination.</p>
     
-    <ol class = "hand-drill-method-list">
-        <li>Make a tinder nest. The tinder nest is necessary to get the fire going. A tinder nest can be made from things
+    <ul class = "hand-drill-method-list">
+        <li>• Make a tinder nest. The tinder nest is necessary to get the fire going. A tinder nest can be made from things
         that bursts into flames effectively, such as to dry leaves, grass, and bark. For more hints on how to pick out the best 
         wood, check out our campfire tips and tricks.</li>
-        <li>Make your notch. Cut an angular notch on your fireboard and create a depression near it.</li>
-        <li>Place bark below the v-shaped cut. The bark is used to get an ember from the erosion between the axle and fireboard.</li>
-        <li>Start spinning. Place the shaft into the groove on your fireboard. Your shaft ought to be around two feet to work well.
+        <li>• Make your notch. Cut an angular notch on your fireboard and create a depression near it.</li>
+        <li>• Place bark below the v-shaped cut. The bark is used to get an ember from the erosion between the axle and fireboard.</li>
+        <li>• Start spinning. Place the shaft into the groove on your fireboard. Your shaft ought to be around two feet to work well.
         Keep up weight on the fireboard and start to roll the shaft in your hands rapidly down the axle. Continue doing this until
         an ember lights on the fireboard.</li>
-        <li>Start a fire. When you see a shining ember, tap the fireboard to place your ember onto the bit of bark. Exchange the bark 
+        <li>• Start a fire. When you see a shining ember, tap the fireboard to place your ember onto the bit of bark. Exchange the bark 
         to your nest of tinder. Slowly blow it to get yourself fire.</li>
-    </ol>
+    </ul>
     </div>
 
     <div class="fire-plow-container">
@@ -113,16 +118,16 @@ export default function Fire() {
     <img src='${firePlow}' alt = "pic of a someone starting a fire by plow method">
     <p class="fire-text">The fire plow is another common method using minimal tools. All you’ll need is a softer wood for the plow board and a harder wood
     for the plow. Willow and poplar make good plow board material.</p>
-
+    
      <ol class = "fire-plow-list">
-     <li>Set up your fireplace and cut a groove about one inch wide and six to eight inches long down a board. Find a piece of hardwood 
+     <li>• Set up your fireplace and cut a groove about one inch wide and six to eight inches long down a board. Find a piece of hardwood 
      about one foot long and carve it to a point at the end.</li>
-     <li>Rub. Take the head of your plow and place it on your fireboard. Begin rubbing the tip of the plow back and forth in the grove.
+     <li>• Rub. Take the head of your plow and place it on your fireboard. Begin rubbing the tip of the plow back and forth in the grove.
       This will create small pits of dust.</li>
-      <li>Raise the board. Raise the top of the board and rest it on your knee, so that the dust collects at the bottom.</li>
-      <li>Intensify the motion. Once a small pile of dust has collected at the bottom, rub the plow quickly and with force in the groove
+      <li>• Raise the board. Raise the top of the board and rest it on your knee, so that the dust collects at the bottom.</li>
+      <li>• Intensify the motion. Once a small pile of dust has collected at the bottom, rub the plow quickly and with force in the groove
        until the dust smolders.</li>
-    <li>Transfer the flame to the tinder. Once the dust is alight, transfer it to the tinder and gently blow to start your fire. </li>
+    <li>• Transfer the flame to the tinder. Once the dust is alight, transfer it to the tinder and gently blow to start your fire. </li>
      </ol>
      </div>
 
@@ -134,26 +139,26 @@ export default function Fire() {
       The items you’ll need for this method are: </p>
 
       <ul class = "fire-bow-drill-materials-list">
-      <li>Fireboard: A flat, softwood board about a foot long and at least six inches wide.</li>
-      <li>Socket: A hand-sized, flat rock with a depression on one side. This is used to put weight on the top of the drill as you’re 
+      <li>• Fireboard: A flat, softwood board about a foot long and at least six inches wide.</li>
+      <li>• Socket: A hand-sized, flat rock with a depression on one side. This is used to put weight on the top of the drill as you’re 
       turning it with the bow.</li>
-      <li>Drill: No, not a power drill! We’re talking a sturdy hardwood stick between one and two inches in diameter and about a foot long.</li>
-      <li>Bow: A sturdy, yet flexible green stick about two feet long and one inch in diameter.</li>
-      <li>Cord: If you don’t have any paracord handy, hiking boot laces make a great cord.</li>
+      <li>• Drill: No, not a power drill! We’re talking a sturdy hardwood stick between one and two inches in diameter and about a foot long.</li>
+      <li>• Bow: A sturdy, yet flexible green stick about two feet long and one inch in diameter.</li>
+      <li>• Cord: If you don’t have any paracord handy, hiking boot laces make a great cord.</li>
       </ul>
     <p class="fire-text">Once you’ve gathered your materials, follow these steps to start the fire.</p>
 
-    <ol class = "fire-bow-drill-list">
-    <li>Make your bow: The bow ought to be about the length of your arm. Bend the bow into a half-moon shape and tie it 
+    <ul class = "fire-bow-drill-list">
+    <li>• Make your bow: The bow ought to be about the length of your arm. Bend the bow into a half-moon shape and tie it 
     into position with the cord.</li>
-    <li>Set up the fireboard. Cut a shallow depression on the center edge of the fireboard. On the underside of the board, 
+    <li>• Set up the fireboard. Cut a shallow depression on the center edge of the fireboard. On the underside of the board, 
     notch a V-shaped cut that meets the depression on top.</li>
-    <li>String up the drill. Circle the bowstring around the drill. Place the drill on the fireboard then apply some
+    <li>• String up the drill. Circle the bowstring around the drill. Place the drill on the fireboard then apply some
      pressure on the other side with your socket.</li>
-     <li>Start sawing. Using your bow, move the drill by doing a sawing motion. The drill should pivot rapidly. Continue 
+     <li>• Start sawing. Using your bow, move the drill by doing a sawing motion. The drill should pivot rapidly. Continue 
      sawing until you make an ember.</li>
-     <li>Start a fire. Put ember into the nest and slowly blow on it to get yourself fire.</li>
-    </ol>
+     <li>• Start a fire. Put ember into the nest and slowly blow on it to get yourself fire.</li>
+    </ul>
     </div>
 
     <div class="fire-stone-container">
@@ -165,11 +170,11 @@ export default function Fire() {
     utilized by various armed forces the world over.</p>
 
       <ol class = "fire-stone-list">
-      <li>Make a tinder nest. This nest will be used to catch the spark that you make with the flint and steel.</li>
-      <li>Grab your starters. Grip the steel in one hand while taking the striker in the other.</li>
-      <li>Strike. Position the fire steel against your base to keep it from moving. At that point push the striker down 
+      <li>• Make a tinder nest. This nest will be used to catch the spark that you make with the flint and steel.</li>
+      <li>• Grab your starters. Grip the steel in one hand while taking the striker in the other.</li>
+      <li>• Strike. Position the fire steel against your base to keep it from moving. At that point push the striker down 
       the length of the fire steel in one smooth movement.</li>
-      <li>Start a fire! Place the ember in the nest and slowly blow it to get yourself a fire.</li>
+      <li>• Start a fire! Place the ember in the nest and slowly blow it to get yourself a fire.</li>
       </ol>
       </div>
 
@@ -179,10 +184,10 @@ export default function Fire() {
       <p class="fire-text">With this strategy, all you need are a few batteries and steel wool.</p>
 
       <ol class = "fire-battery-list">
-      <li>Stretch out the wool. You need it to be around 6 inches in length and at least an inch wide.</li>
-      <li>Rub the battery on the steel wool. You can use any battery; however 9-volt batteries are recommended.
+      <li>• Stretch out the wool. You need it to be around 6 inches in length and at least an inch wide.</li>
+      <li>• Rub the battery on the steel wool. You can use any battery; however 9-volt batteries are recommended.
        Rub the battery sides with the steel. The wool will start to gleam and blaze.</li>
-       <li>Place the blazing wool on the tinder nest and slowly blow it to get a larger fire.</li>
+       <li>• Place the blazing wool on the tinder nest and slowly blow it to get a larger fire.</li>
       </ol>
       </div>
 
@@ -193,16 +198,16 @@ export default function Fire() {
       binocular lens both work.</p>
 
       <ol class = "fire-lenses-list">
-      <li>Get your tinder ready.</li>
-      <li>Hold the magnifying glass between the sun and the tinder. Look for the bright dot that appears. Tilt the
+      <li>• Get your tinder ready.</li>
+      <li>• Hold the magnifying glass between the sun and the tinder. Look for the bright dot that appears. Tilt the
        magnifying glass so that the dot is over the tinder and about a quarter-inch in diameter.</li>
-       <li>Focus the dot for 30 seconds to a minute. As with other methods of fire starting, patience is key! Once the
+       <li>• Focus the dot for 30 seconds to a minute. As with other methods of fire starting, patience is key! Once the
         tinder starts smoking, gently blow to get the flame going.</li>
       </ol>
       </div>
 
       <div class ="skills-main">
-        <h3>Take a Quiz on how to build a Fire</h3>
+        <h3 class="fire-quiz-title">Take a Quiz on how to build a Fire</h3>
         <button class="quiz-btn-links_Fire">Quiz</button>
       </div>
 
