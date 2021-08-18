1e55e17f72108efc787b2d56de96ba8787319753
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-08-18 13:35:02 -0400

    update blog css to be responsive

--- client-side/js/components/Footer.js
+++ client-side/js/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return `
-    <p class="footer"><small>&copy Wilderness Wild & WCCI 2021</small></p>  
+    <p class="footer"><small>&copy Wilderness Wild 🌳 & WCCI 2021</small></p>  
    
     `;
 }
--- client-side/js/components/Home.js
+++ client-side/js/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   <img class="logo" id="logo" src="${WildernessWildLogo}" alt="wilderness Wild Logo">
   <br>
   <br>
-  <p>Wilderness Wild is a nonprofit organization dedicated to improving the lives by providing the trustworthy information, education, and independent voice related to survival skills in the wilderness.</p>
+  <p><strong class="fire-quiz-title">Wilderness Wild 🌳</strong> is a nonprofit organization dedicated to improving the lives by providing the trustworthy information, education, and independent voice related to survival skills in the wilderness.</p>
   </div>
   `;
 }
