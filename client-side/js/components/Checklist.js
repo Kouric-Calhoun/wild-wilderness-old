9efe190fc3351a8730f2fbc3eb3cf4fa4d3ac3b7
FilsanMadar <filsanmadar@gmail.com>
2021-08-19 22:56:07 -0400

    css issues with checklist

--- client-side/js/components/Checklist.js
+++ client-side/js/components/Checklist.js
@@ -4,6 +4,7 @@ export default function Checklist() {
     <header class="checklist__todo">
       <h1 class="checklist__todo"> To Do List</h1>
     </header>
+    <div class="pull-left" style="margin-right:5px">
     <form class="checklist">
       <input type="text" class="todo-input" />
       <button class="todo-button" type="submit">
@@ -17,6 +18,7 @@ export default function Checklist() {
         </select>
       </div>
     </form>
+    </div>
     <div class="todo-container">
       <ul class="todo-list"></ul>
     </div>
