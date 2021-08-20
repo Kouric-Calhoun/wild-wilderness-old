fa00f423d240aa6599cbb2951f913ea054d3f126
Kouric-Calhoun <kouriccalhoun@gmail.com>
2021-08-20 15:44:49 -0400

    style for region section

--- client-side/js/pages/Regions/Southwest.js
+++ client-side/js/pages/Regions/Southwest.js
@@ -1,6 +1,7 @@
 export default function Southwest() {
   return `
       <div class ="region-text">
+      <h1>Southwest Region</h1>
       The five regions of the United States all have many things that make them unique. Although the Southwest is only made up of four states, they are very large. The Southwest region is known for its vast deserts, delicious food, the Grand Canyon, and rich culture. 
       Let’s learn all about what makes the Southwest so unique, and an amazing place to visit or live! Some of the most useful natural resources from the Southwest region are copper, sliver, iron, coal, and oil. The Southwest is the leading region in the raising of beef cattle and sheep. 
       The climate in the Southwest is hot and dry with very little rain. The weather very rarely changes, and droughts are common due to the lack of rainfall.
@@ -14,9 +15,10 @@ export default function Southwest() {
       <div class="park">
         <h3>Grand Canyon National Park</h3>
         <p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26406491.41850018!2d-129.48233175488062!3d36.106925800000006!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1f38a9bec9912029!2sGrand%20Canyon%20National%20Park!5e0!3m2!1sen!2sus!4v1629061314497!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26406491.41850018!2d-129.48233175488062!3d36.106925800000006!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1f38a9bec9912029!2sGrand%20Canyon%20National%20Park!5e0!3m2!1sen!2sus!4v1629061314497!5m2!1sen!2sus" width="290" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </p>
         <address>Arizona</address>
+        </div>
 
       
       </div>
@@ -24,14 +26,14 @@ export default function Southwest() {
       <div class="park">
         <h3>Arch Rock Campground</h3>
         <p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26301345.88401779!2d-132.47784941423419!3d36.418545099999996!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd5e7cecef1b56049!2sArch%20Rock%20Campground!5e0!3m2!1sen!2sus!4v1629061494752!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26301345.88401779!2d-132.47784941423419!3d36.418545099999996!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd5e7cecef1b56049!2sArch%20Rock%20Campground!5e0!3m2!1sen!2sus!4v1629061494752!5m2!1sen!2sus" width="290" height="290" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </p>
         <address>Unnamed Road, Overton, NV 89040</address>
       </div>
     </div>
 
     <div id="scenarios-div">
-            <h1>Everything About Survival in the South West</h1>
+            <h1> Survival Game</h1>
             <div class="scenarios-splash">
               <h3 class="scenarios-p"><em>Survival Gears, Guides, and Advice</em></h3>
               <p id="scenarios-text">While the great outdoors is beautiful, you may find yourself in a situation that could lead to a very bad ending if you are not prepared. 
@@ -45,8 +47,8 @@ export default function Southwest() {
               <h3>Scenario 3 to add</h3>
               <button class = "home-btn-links"> Scenario 3 </button>
             </div>
-            <div class="southeast">Southeast</div>
-            <div class="westalaskahawaii">Alaska & Hawaii</div>
+            <div class="southeast"><< Southeast</div>
+            <div class="westalaskahawaii">Alaska & Hawaii >></div>
           </div>
           `;
 }
